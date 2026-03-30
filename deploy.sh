e3f43cf5af764fbe23be4cf4f2d60d66e3c853a0
Imaging Automation <RD_Imaging_Team_X-ray@sap.com>
2026-03-29 23:10:09 -0700

    Fix deploy.sh PATH for Methuselah's non-login SSH shell

--- deploy.sh
+++ deploy.sh
@@ -6,5 +6,5 @@ REMOTE="ptgomez@methuselah.local"
 DEPLOY_DIR="~/foodplan"
 
 echo "Deploying FoodPlan to Methuselah..."
-ssh "$REMOTE" "cd $DEPLOY_DIR && git pull origin v0.1 && docker compose -f docker-compose.yml -f docker-compose.prod.yml up -d --build"
+ssh "$REMOTE" "export PATH=/usr/local/bin:\$PATH && cd $DEPLOY_DIR && git pull origin v0.1 && docker compose -f docker-compose.yml -f docker-compose.prod.yml up -d --build"
 echo "Deploy complete. App available at http://methuselah.local:3001/"
